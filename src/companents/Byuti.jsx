5e5122ec8a9ae2d0b43f794e8fa69a47e7bd87e2
omina08 <faxriddinovaomina8@email.com>
2025-09-24 21:17:39 +0500

    Sayt tugatildi

--- src/companents/Byuti.jsx
+++ src/companents/Byuti.jsx
@@ -6,56 +6,66 @@ const Byuti = () => {
 
   return (
     <>
-      <div className='bg-[#363348] text-white flex flex-col lg:flex-row gap-10 lg:gap-[100px] items-center px-6 sm:px-10 md:px-20 lg:px-40 py-10 lg:h-[600px] relative'>
+      <div className='bg-[#363348] text-white relative'>
+        <div className='container mx-auto px-6 sm:px-10 md:px-20 lg:px-40 py-10 lg:py-20 flex flex-col lg:flex-row items-center lg:items-start gap-10 lg:gap-16'>
 
-        {/* Matn blok */}
-        <div className='text-center lg:text-left max-w-lg'>
-          <h1 className='text-2xl sm:text-3xl lg:text-4xl font-light mb-4 font-seri leading-snug'>
-            {t("byuti.title1")} <br />
-            {t("byuti.title2")} <br />
-            <span className='text-[#C88C61]'>
-              {t("byuti.title3")} <br /> {t("byuti.title4")}
-            </span>
-          </h1>
+          {/* Matn blok */}
+          <div className='text-center lg:text-left max-w-lg lg:max-w-md flex-shrink-0'>
+            <h1 className='text-2xl sm:text-3xl lg:text-4xl font-light mb-4 leading-snug'>
+              {t("byuti.title1")} <br />
+              {t("byuti.title2")} <br />
+              <span className='text-[#C88C61]'>
+                {t("byuti.title3")} <br /> {t("byuti.title4")}
+              </span>
+            </h1>
 
-          <p className='text-sm mb-4 max-w-md mx-auto lg:mx-0'>
-            {t("byuti.desc")}
-          </p>
+            <p className='text-sm sm:text-base mb-4 max-w-md mx-auto lg:mx-0'>
+              {t("byuti.desc")}
+            </p>
 
-          <h1 className='text-lg sm:text-xl mb-4'>
-            {t("byuti.subtitle")}
-          </h1>
+            <h2 className='text-lg sm:text-xl mb-4'>
+              {t("byuti.subtitle")}
+            </h2>
 
-          <div className='flex items-center w-full max-w-sm mx-auto lg:mx-0 h-[60px] mb-4'>
-            <img className='bg-white rounded-l-md py-2 px-3' src="/phone.png" alt="" />
-            <input
-              className='p-2 flex-1 rounded-r-md text-gray-700 bg-white'
-              type="text"
-              placeholder={t("byuti.placeholder")}
+            {/* Input va telefon */}
+            <div className='flex items-center w-full max-w-sm mx-auto lg:mx-0 h-[60px] mb-4'>
+              <img className='bg-white rounded-l-md py-2 px-3' src="/phone.png" alt="" />
+              <input
+                className='p-3 flex-1 rounded-r-md text-gray-700 bg-white'
+                type="text"
+                placeholder={t("byuti.placeholder")}
+              />
+            </div>
+
+            <button className='w-full max-w-sm mx-auto lg:mx-0 py-4 px-10 text-white rounded-md bg-[#C88C61] hover:bg-[#EBBE9C] transition'>
+              {t("byuti.button")}
+            </button>
+          </div>
+
+          {/* Rasm blok */}
+          <div className='relative w-full flex justify-center lg:justify-end'>
+            <img
+              className='w-64 sm:w-96 md:w-[500px] lg:w-[700px] relative'
+              src="/chair.png"
+              alt="chair"
+            />
+            <img
+              className='absolute w-16 sm:w-20 md:w-28 lg:w-36 hover:scale-105 transition cursor-pointer'
+              src="/button play.png"
+              alt="play"
+              style={{
+                bottom: '15%',
+                left: '50%',
+                transform: 'translateX(-50%)'
+              }}
             />
           </div>
 
-          <button className='w-full max-w-sm mx-auto lg:mx-0 py-4 px-10 text-white rounded-md bg-[#C88C61] hover:bg-[#EBBE9C] transition'>
-            {t("byuti.button")}
-          </button>
         </div>
 
-        {/* Video play tugmasi */}
-        <img
-          className='absolute lg:static lg:left-1/3 w-20 sm:w-24 md:w-28 lg:w-36 z-10 bottom-10 lg:bottom-20 hover:scale-105 transition'
-          src="/button play.png"
-          alt=""
-        />
-
-        {/* Fon rasmi */}
-        <img
-          className='w-64 sm:w-96 md:w-[500px] lg:w-[700px] mb-10 lg:mb-0'
-          src="/chair.png"
-          alt="chair"
-        />
+        {/* Past bo‘sh joy */}
+        <div className='h-20 md:h-24 lg:h-28'></div>
       </div>
-
-      <div className='h-10 md:h-20 bg-[#363348]'></div>
     </>
   )
 }
